9959db0e25a6ce330f7048a8f36677e169e72be0
Aymen Ben Othmen <aymenbenothmenabo@gmail.com>
2019-04-20 17:17:49 +0100

    test: test singleton metaclass

---
+++ tests/lib/services/service.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from lib.services.container import Singleton
+
+
+class Service(metaclass=Singleton):
+    def __init__(self):
+        self.x = 5
+
+
+class IServiceTest(TestCase):
+
+    def test_is_singleton(self):
+        a = Service()
+        b = Service()
+
+        b.x = 10
+
+        self.assertEqual(a.x, b.x, 'Attribute values are not equal!')
+        self.assertEqual(a, b, 'Objects are not equal!')
