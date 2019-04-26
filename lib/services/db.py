ec50ccf63c7a84ab954220f3962ac5689c5b3eb9
Aymen Ben Othmen <aymenbenothmenabo@gmail.com>
2019-04-26 13:41:14 +0100

    feat: define get and new project

--- lib/services/db.py
+++ lib/services/db.py
@@ -2,22 +2,25 @@ from lib.services.idb import IDataBase
 from pymongo import MongoClient
 
 
-class MongoDB(IDataBase):
+class MongodbORM(IDataBase):
 
-    def __init__(self, project_id, experience_id, *args, **kwargs):
+    def __init__(self):
         # todo ask for user username and password to protect database.
-        self.client = MongoClient('localhost', 27017)
-        self.project_id = project_id
-        self.experience_id = experience_id
+        self.client = MongoClient('172.18.0.100', 27017)
+        self.database = self.client['akk']
+        self.projects = self.database.project
+        self.experiences = self.database.experience
+        self.search_spaces = self.database.search_space
 
     def authenticate(self):
         pass
 
-    def get_project(self, project_id):
-        pass
+    def get_project(self, path):
+        return self.projects.find_one({'path': path})
 
-    def new_project(self, project_id):
-        pass
+    def new_project(self, project: dict):
+        project_id = self.projects.insert_one(project).inserted_id
+        return project_id
 
     def new_experience(self, project_id):
         pass
