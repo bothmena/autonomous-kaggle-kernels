e4418e8213bea86dc00150332c839f334bf3f835
Aymen Ben Othmen <aymenbenothmenabo@gmail.com>
2019-04-26 19:12:34 +0100

    test: unique project name/path tests

--- tests/lib/services/mongo.py
+++ tests/lib/services/mongo.py
@@ -1,8 +1,19 @@
 from unittest import TestCase
 from lib.services.db import MongodbORM
+from lib.exception.database import ProjectExistsException
 
 
 class MongoDBTest(TestCase):
+
+    project = {
+        'name': 'test project',
+        'path': '/workspace/BrighterAI/autonomous-kaggle-kernels',
+        'repository': 'None',
+        'framework': 'tensorflow',
+        'cpu': False,
+        'internet': False,
+    }
+    db = MongodbORM()
 
     def test_singleton(self):
         a = MongodbORM()
@@ -11,42 +22,34 @@ class MongoDBTest(TestCase):
         self.assertEqual(a, b)
 
     def test_new_project(self):
-        db = MongodbORM()
-        project = {
-            'name': 'test project',
-            'path': '/workspace/BrighterAI/autonomous-kaggle-kernels',
-            'repository': 'None',
-            'framework': 'tensorflow',
-            'cpu': False,
-            'internet': False,
-        }
-        uid = db.new_project(project)
+        uid = self.db.new_project(self.project)
 
-        project_db = db.projects.find_one({'_id': uid})
+        project_db = self.db.projects.find_one({'_id': uid})
 
-        self.assertEqual(project['name'], project_db['name'])
-        self.assertEqual(project['path'], project_db['path'])
-        self.assertEqual(project['repository'], project_db['repository'])
-        self.assertEqual(project['framework'], project_db['framework'])
-        self.assertEqual(project['cpu'], project_db['cpu'])
-        self.assertEqual(project['internet'], project_db['internet'])
+        self.assertEqual(self.project['name'], project_db['name'])
+        self.assertEqual(self.project['path'], project_db['path'])
+        self.assertEqual(self.project['repository'], project_db['repository'])
+        self.assertEqual(self.project['framework'], project_db['framework'])
+        self.assertEqual(self.project['cpu'], project_db['cpu'])
+        self.assertEqual(self.project['internet'], project_db['internet'])
 
-    def test_find_project(self):
-        db = MongodbORM()
-        project = {
-            'name': 'test project',
-            'path': '/workspace/BrighterAI/autonomous-kaggle-kernels',
-            'repository': 'None',
-            'framework': 'tensorflow',
-            'cpu': False,
-            'internet': False,
-        }
-        db.new_project(project)
-        project_db = db.get_project(project['path'])
+        self.db.projects.delete_one({'_id': uid})
 
-        self.assertEqual(project['name'], project_db['name'])
-        self.assertEqual(project['path'], project_db['path'])
-        self.assertEqual(project['repository'], project_db['repository'])
-        self.assertEqual(project['framework'], project_db['framework'])
-        self.assertEqual(project['cpu'], project_db['cpu'])
-        self.assertEqual(project['internet'], project_db['internet'])
+    def test_get_project(self):
+        uid = self.db.new_project(self.project)
+        project_db = self.db.get_project(self.project['path'])
+
+        self.assertEqual(self.project['name'], project_db['name'])
+        self.assertEqual(self.project['path'], project_db['path'])
+        self.assertEqual(self.project['repository'], project_db['repository'])
+        self.assertEqual(self.project['framework'], project_db['framework'])
+        self.assertEqual(self.project['cpu'], project_db['cpu'])
+        self.assertEqual(self.project['internet'], project_db['internet'])
+
+        self.db.projects.delete_one({'_id': uid})
+
+    def test_unique_name(self):
+        uid = self.db.new_project(self.project)
+        self.assertRaises(ProjectExistsException, self.db.new_project, self.project)
+
+        self.db.projects.delete_one({'_id': uid})
