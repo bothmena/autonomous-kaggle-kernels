363b229d5676f7aebfaf8a341c9ec9b3ed099875
Aymen Ben Othmen <aymenbenothmenabo@gmail.com>
2019-04-29 14:42:43 +0100

    feat: add an exception for undefined required HPs

--- lib/exception/implementation.py
+++ lib/exception/implementation.py
@@ -17,3 +17,16 @@ class ManyExperiencesException(ImplementationException):
         super(ManyExperiencesException, self).__init__(message, status)
         self.message = message
         self.status = status
+
+
+class HPNotDefinedException(ImplementationException):
+    def __init__(self, message: str = 'A Hyper parameter is not defined in the experience', status=None, hp: str = None, net_id: str = None):
+        if hp is not None:
+            if net_id is None:
+                self.message = 'the {} is not defined in the experience'.format(hp)
+            else:
+                self.message = 'the {} for network {} is not defined in the experience'.format(hp, net_id)
+        else:
+            self.message = message
+        super(HPNotDefinedException, self).__init__(message, status)
+        self.status = status
