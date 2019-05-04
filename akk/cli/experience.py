069b852508ccbb05303136e4eb523a70038c88b7
Aymen Ben Othmen <aymenbenothmenabo@gmail.com>
2019-05-04 22:37:09 +0100

    fix: fix kernel push authentication error

--- akk/cli/experience.py
+++ akk/cli/experience.py
@@ -1,64 +1,30 @@
-import os
 import json
+import os
 
 from git.exc import InvalidGitRepositoryError
+from kaggle.api.kaggle_api_extended import KaggleApi
+from kaggle.api_client import ApiClient
+from slugify import slugify
+from bson import ObjectId
 
-from akk.lib.exception import NoRepoException, UncommitedChangesException, ExperienceExistsException
+from akk.lib.exception import NoRepoException, UncommitedChangesException, ExperienceExistsException, \
+    ExperienceNotFoundException, ProjectNotAssembledException
 from akk.lib.services import MongodbORM
-from akk.lib.utils import CodeSourceImporter
-from akk.lib.utils.helpers import get_experiences, get_project_last_commit, is_exp_valid
 from akk.lib.utils import py2nb
-from kaggle.api import KaggleApi
+from akk.lib.utils.helpers import get_experiences, get_project_last_commit, is_exp_valid, project_not_found
 
 
 orm = MongodbORM()
-kaggle = KaggleApi()
 
-
-def _fill_experience(experience: dict):
-    """
-    if the optional keys: ['lr_decay', 'lr_cycle', 'opt_args', 'loss_args'] are not defined in the experience
-    we should set them at their default values, respectfully: [None, None, {}, {}]
-    :param experience: the experience that will be saved in the DB
-    :return:
-    """
-    keys = ['lr_decay', 'lr_cycle', 'opt_args', 'loss_args']
-    defaults = [None, None, {}, {}]
-
-    for i in range(4):
-        if keys[i] not in experience.keys():
-            experience[keys[i]] = defaults[i]
-
-    return experience
-
-
-def _project_not_found():
-    print('This directory is not a registered AKK project, please make sure you are issuing the cli from the right directory.')
-    print('If you did not initialize the project all you have to do is to run:')
-    print('$ akk project init [-h: for more information]')
-
-
-def _assemble_code(project, output_dir):
-    # todo: add new attribute to project and new cli argument to project init: entrypoint: main .py file, default: main.py
-    project['entrypoint'] = 'main.py'
-    importer = CodeSourceImporter(project['entrypoint'], project['path'], output_dir)
-    importer.find_file_deps()
-    importer.write_output()
+kaggle = KaggleApi(ApiClient())
+kaggle.authenticate()
 
 
 def _write_exp_data(experience, path, last_commit):
-    if not os.path.isdir(os.path.join(path, '.akk', last_commit, 'experiences')):
-        os.mkdir(os.path.join(path, '.akk', last_commit, 'experiences'), 0o755)
-    if not os.path.isdir(os.path.join(path, '.akk', last_commit, 'experiences', str(experience['_id']))):
-        os.mkdir(os.path.join(path, '.akk', last_commit, 'experiences', str(experience['_id'])), 0o755)
-
     temp_filename = os.path.join(path, '.akk', last_commit, 'experiences', str(experience['_id']), 'script.py')
 
     with open(temp_filename, 'w') as temp:
         dic_next = False
-        prev_space = 0
-        pprev_space = 0
-        ppprev_space = 0
         with open(os.path.join(path, '.akk', last_commit, 'output.py'), 'r') as f:
             for line in f.readlines():
                 if line == '# __EXP__\n':
@@ -79,35 +45,33 @@ def _write_exp_data(experience, path, last_commit):
     os.chmod(temp_filename, 0o777)
 
 
-def _slugify(st: str) -> str:
-    return ' '.join(st.rstrip().split()).lower().replace(' ', '-')
-
-
 def _kernel_metadata(project: dict, experience: dict):
-    name = project['name'] + ' ' + str(experience['_id'])
-    metadata = {
-        "id"                 : "bothmena/" + _slugify(name),
-        "title"              : name,
-        "code_file"          : "script.ipynb",
-        "language"           : "python",
-        "kernel_type"        : project['type'],
-        "is_private"         : project['private'],
-        "enable_gpu"         : not project['cpu'],
-        "enable_internet"    : project['internet'],
-        "dataset_sources"    : project['datasets'],  # ["bothmena/my-awesome-dataset"]
-        "competition_sources": project['competitions'],
-        "kernel_sources"     : project['kernels'] + ["bothmena/" + _slugify(name)]
+    name = project['alias'] + ' ' + str(experience['_id'])
+    meta_data = {
+        'id'                 : 'bothmena/' + slugify(name),
+        'title'              : name,
+        'code_file'          : 'script.ipynb',
+        'language'           : 'python',
+        'kernel_type'        : project['type'],
+        'is_private'         : 'true' if project['private'] else 'false',
+        'enable_gpu'         : 'false' if project['cpu'] else 'true',
+        'enable_internet'    : 'true' if project['internet'] else 'false',
+        'dataset_sources'    : project['datasets'],
+        'competition_sources': project['competitions'],
+        'kernel_sources'     : ["bothmena/" + slugify(name)] + project['kernels']
     }
-    with open(os.path.join(project['path'], '.akk', experience['git_commit'], 'experiences', str(experience['_id']), 'kernel-metadata.json'), 'w') as f:
-        json.dump(metadata, f)
+    meta_file = os.path.join(os.path.join(project['path'], '.akk', experience['git_commit'], 'experiences', str(experience['_id']), KaggleApi.KERNEL_METADATA_FILE))
+    with open(meta_file, 'w') as f:
+        json.dump(meta_data, f, indent=2)
 
 
 # def new_project_handler(group=None, category=None, sort_by=None, page=1, search=None, csv_display=False):
 def new_exp(filename: str):
+    # todo: remove experiences from previous project git_commit that were not yet started.
     path = os.getcwd()
     project = orm.get_project(path)
     if project is None:
-        _project_not_found()
+        project_not_found()
     else:
         experiences = get_experiences(filename)
         successes = 0
@@ -124,7 +88,7 @@ def new_exp(filename: str):
                 experience['project'] = str(project['_id'])
                 experience['git_commit'] = last_commit
                 experience['search_space'] = None
-                experience['status'] = 'Not started'  # profiling / running / stopped / completed / failed / queued (when using search space and set limit of parallel experiences)
+                experience['status'] = 'unstarted'  # profiling / running / stopped / completed / failed / queued (when using search space and set limit of parallel experiences)
                 try:
                     orm.new_experience(experience)
                     successes += 1
@@ -140,7 +104,7 @@ def start_exp(exp_id, *args, **kwargs):
     path = os.getcwd()
     project = orm.get_project(path)
     if project is None:
-        _project_not_found()
+        project_not_found()
         return
 
     # get experience with id and project id.
@@ -149,49 +113,50 @@ def start_exp(exp_id, *args, **kwargs):
         print('This project has no experience with id:', exp_id)
         return
 
-    try:
-        # check project last commit, else warning
-        last_commit = get_project_last_commit(path)
-        if last_commit is None:
-            raise UncommitedChangesException()
+    # if an experience marked as stopped, mark it as running and let the watchdog commit the code automatically.
+    if experience['status'] == 'stopped':
+        orm.experiences.update_one({'_id': experience['_id']}, {'$set': {'status': 'running'}})
+        print('Experience restarted.')
+        return
 
-        # check if the directory "project_path/.akk/_last_commit_id_" exists, if not create it.
-        if not os.path.isdir(os.path.join(path, '.akk', last_commit)):
-            os.mkdir(os.path.join(path, '.akk', last_commit), 0o755)
-        # check if assembled file exists in: project_path/.akk/_last_commit_id_/output.py, if not assemble code.
-        if not os.path.isfile(os.path.join(path, '.akk', last_commit, 'output.py')):
-            _assemble_code(project, os.path.join(path, '.akk', last_commit))
-    except InvalidGitRepositoryError:
-        raise NoRepoException()
+    # you can only start an experience that is unstarted, queued or failed
+    # a queued exp. is an experience expanded from a search space and marked as queued if not started immediately
+    # an experience can fail due to two reasons: a bug in the project code -> fix the bug, commit, create new experience and start it.
+    # or because a problem in estimating the time of execution of the cycles -> fix the akk library -> restart the experience.
+    if experience['status'] not in ['unstarted', 'queued', 'failed']:
+        print('This experience has already started!')
+        return
+
+    # check if assembled file exists in: project_path/.akk/_last_commit_id_/output.py
+    if not os.path.isfile(os.path.join(path, '.akk', experience['git_commit'], 'output.py')):
+        raise ProjectNotAssembledException()
+
+    if not os.path.isdir(os.path.join(path, '.akk', experience['git_commit'], 'experiences', str(experience['_id']))):
+        os.makedirs(os.path.join(path, '.akk', experience['git_commit'], 'experiences', str(experience['_id'])), 0o755)
 
     # create profiling commit for the experience
     commit = {'experience': experience['_id'], 'cycles': {}}
     for name, cycle in experience['cycles'].items():
         commit['cycles'][name] = {'steps': 20, 'max_runtime': None}
 
-    orm.new_commit(commit)
+    commit_id = orm.new_commit(commit)
 
     # replace experience cycles with profiling commit cycles dict (where all cycles.steps = 50)
     experience_cp = experience.copy()
     experience_cp['cycles'] = commit['cycles']
 
-    # todo: uncomment next line
-    # _write_exp_data(experience_cp, path, last_commit)
+    _write_exp_data(experience_cp, path, experience['git_commit'])
 
     # create a kernel meta-data
-    # todo: uncomment next line
-    # _kernel_metadata(project, experience)
+    _kernel_metadata(project, experience)
 
-    # akk p init -n 'AKK - Udacity Facial Keypoints Detection Project' --internet
     # convert script to notebook
-    folder = os.path.join(path, '.akk', last_commit, 'experiences', str(experience['_id']))
-    # todo: uncomment next line
-    # py2nb.convert(folder)
+    folder = os.path.join(path, '.akk', experience['git_commit'], 'experiences', str(experience['_id']))
+    py2nb.convert(folder)
 
-    # todo: use kaggle api to push the code.
     kaggle.kernels_push_cli(folder)
-
-    # todo: split into commits and run sequentially.
+    orm.commits.update_one({'_id': commit_id}, {'$set': {'status': 'running'}})
+    orm.experiences.update_one({'_id': experience['_id']}, {'$set': {'status': 'profiling'}})
 
 
 def stop_exp(*args, **kwargs):
@@ -204,7 +169,7 @@ def list_exp(*args, **kwargs):
     path = os.getcwd()
     project = orm.get_project(path)
     if project is None:
-        _project_not_found()
+        project_not_found()
     else:
         print('')
         print(' ' + '-' * 98)
@@ -224,7 +189,24 @@ def list_exp(*args, **kwargs):
         print('')
 
 
-def status_exp(*args, **kwargs):
-    print('-' * 50)
-    print('experience status cli / Not yet implemented')
-    print('-' * 50)
+def status_exp(experience: str):
+    exp = orm.get_experience(experience)
+    if exp is None:
+        raise ExperienceNotFoundException()
+
+    commit = orm.commits.find_one({'index': 0, 'experience': exp['_id']})
+
+    print('')
+    print(' Status: ', exp['status'])
+    print('\n ' + '-' * 42)
+    print(' | {:2s} | {:15s} | {:15s} |'.format('#', 'Focused', 'Status'))
+    print(' ' + '-' * 42)
+    i = 0
+    while commit is not None:
+        i += 1
+        print(' | {:2d} | {:15s} | {:15s} |'.format(i, 'Yes' if commit['focus'] else 'No', commit['status'].upper()))
+        print(' ' + '-' * 42)
+        if commit['next'] is None:
+            commit = None
+        else:
+            commit = orm.get_commit(str(commit['next']))
