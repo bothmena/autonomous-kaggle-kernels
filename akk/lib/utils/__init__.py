4a6493819ac97dd606de3df34765e4b2b0aaa265
Aymen Ben Othmen <aymenbenothmenabo@gmail.com>
2019-05-06 00:47:18 +0100

    fix: kaggle config file not found

--- akk/lib/utils/__init__.py
+++ akk/lib/utils/__init__.py
@@ -1,10 +1,11 @@
 from .source_code import CodeSourceImporter
-from kaggle.api.kaggle_api_extended import KaggleApi
 from kaggle.api_client import ApiClient
 
 
-kaggle = KaggleApi(ApiClient())
 try:
+    from kaggle.api.kaggle_api_extended import KaggleApi
+
+    kaggle = KaggleApi(ApiClient())
     kaggle.authenticate()
 except OSError:
-    pass
+    print('Could not find kaggle.json.')
