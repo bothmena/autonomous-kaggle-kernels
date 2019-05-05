bad3f122a3b997e3c7d0c5ca4bd06252292e52e4
Aymen Ben Othmen <aymenbenothmenabo@gmail.com>
2019-05-05 01:13:07 +0100

    fix: add dependencies to avoid installation problems

--- setup.py
+++ setup.py
@@ -13,11 +13,15 @@ setuptools.setup(
     keywords=['Autonomous', 'Kernel', 'Kaggle', 'API'],
     packages=setuptools.find_packages(exclude=["src", "kaggle-api", "examples", "tests"]),
     entry_points={'console_scripts': ['akk = akk.cli.commands:main']},
+    install_requires=[
+        'gitpython >= 2.1.*',
+        'python-slugify'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )
-
+# gitpython
 # rm -rf akk.egg-info/ build/ dist/ && pip uninstall akk -y && python3 setup.py sdist bdist_wheel && pip install dist/akk-0.1-py3-none-any.whl
